d693cfd65849debbc8f7eca7aeff1bb3c33597fe
dengxiaofeng <1186313486@qq.com>
2021-04-27 10:04:18 +0800

    [build] 2.4.3

--- src/apis.ts
+++ src/apis.ts
@@ -14,7 +14,7 @@ import type {
   ObjectType,
   RegistrableApp,
 } from './interfaces';
-/* */
+/* 5 */
 import type { ParcelConfigObjectGetter } from './loader';
 import { loadApp } from './loader';
 import { doPrefetchStrategy } from './prefetch';
