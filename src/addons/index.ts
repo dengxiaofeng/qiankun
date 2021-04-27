1f6806fe077ab3fc4eceedc30933db097133105b
dengxiaofeng <1186313486@qq.com>
2021-04-27 09:32:03 +0800

    updrage qiankun version 2.4.1

--- src/addons/index.ts
+++ src/addons/index.ts
@@ -9,6 +9,7 @@ import type { FrameworkLifeCycles, ObjectType } from '../interfaces';
 import getRuntimePublicPathAddOn from './runtimePublicPath';
 import getEngineFlagAddon from './engineFlag';
 
+/* es */
 export default function getAddOns<T extends ObjectType>(global: Window, publicPath: string): FrameworkLifeCycles<T> {
   return mergeWith({}, getEngineFlagAddon(global), getRuntimePublicPathAddOn(global, publicPath), (v1, v2) =>
     concat(v1 ?? [], v2 ?? []),
