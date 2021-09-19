91db067ae9f667079d2e17a11d1e1989a952b704
dengxiaofeng <1186313486@qq.com>
2021-09-19 11:17:29 +0800

    [build] 2.14.20

--- src/globalState.ts
+++ src/globalState.ts
@@ -11,7 +11,6 @@ let globalState: Record<string, any> = {};
 const deps: Record<string, OnGlobalStateChangeCallback> = {};
 
 
-
 // 触发全局监听
 function emitGlobal(state: Record<string, any>, prevState: Record<string, any>) {
   Object.keys(deps).forEach((id: string) => {
